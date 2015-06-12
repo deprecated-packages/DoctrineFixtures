6d1f40d3ed2c95cff1b69df74d1baae61bb9639d
Tomas Votruba <tomas.vot@gmail.com>
2015-06-12 13:29:26 +0200

    Contract added

---
+++ src/Contract/Alice/AliceLoaderInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file is part of Zenify
+ * Copyright (c) 2012 Tomas Votruba (http://tomasvotruba.cz)
+ */
+
+namespace Zenify\DoctrineFixtures\Contract\Alice;
+
+
+interface AliceLoaderInterface
+{
+
+}
---
+++ src/Contract/DataFixtures/DataFixturesLoaderInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of DoctrineFixtures
+ *
+ * Copyright (c) 2014 Pears Health Cyber, s.r.o. (http://pearshealthcyber.cz)
+ *
+ * For the full copyright and license information, please view
+ * the file license.md that was distributed with this source code.
+ */
+
+class DataFixturesLoaderInterface
+{
+
+}
