7f5dbc65d11d352f0170784da0b8afd684c05570
Tomas Votruba <tomas.vot@gmail.com>
2015-06-12 13:29:41 +0200

    Exceptions added

---
+++ src/Exception/MissingDirException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of Zenify
+ * Copyright (c) 2012 Tomas Votruba (http://tomasvotruba.cz)
+ */
+
+namespace Zenify\DoctrineFixtures\Exception;
+
+use Exception;
+
+
+class MissingDirException extends Exception
+{
+
+}
